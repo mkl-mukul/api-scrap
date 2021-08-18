7a1009889ca4f23b35d41e3d6e81b4f611aeadca
mkl-mukul <mukulmakwana09@gmail.com>
2021-08-18 21:53:38 +0530

    updated database

--- _api/views.py
+++ _api/views.py
@@ -7,30 +7,30 @@ from .models import Api
 import requests
 # Create your views here.
 
-r = requests.get("https://www.imdb.com/chart/top?ref_=nv_mv_250")
-soup = BeautifulSoup (r.content,'lxml') 
-link_list=[]
-# links =soup.find('tbody') 
-title=soup.find_all('td',attrs = {'class':'titleColumn'})
-ratings=soup.find_all('td',attrs={'class':'ratingColumn imdbRating'})
-#data1={'title':[],'ratings':[],'year':[]}
-# for r in range(len(title)):
-#     data1['title'].append(title[r].a.text)
-#     data1['year'].append(title[r].span.text)
-#     data1['ratings'].append(ratings[r].strong.text)
+# r = requests.get("https://www.imdb.com/chart/top?ref_=nv_mv_250")
+# soup = BeautifulSoup (r.content,'lxml') 
+# link_list=[]
+# # links =soup.find('tbody') 
+# title=soup.find_all('td',attrs = {'class':'titleColumn'})
+# ratings=soup.find_all('td',attrs={'class':'ratingColumn imdbRating'})
+# #data1={'title':[],'ratings':[],'year':[]}
+# # for r in range(len(title)):
+# #     data1['title'].append(title[r].a.text)
+# #     data1['year'].append(title[r].span.text)
+# #     data1['ratings'].append(ratings[r].strong.text)
 
-for i in range(len(title)):       
-    link_list.append(title[i].a.get('href'))
+# for i in range(len(title)):       
+#     link_list.append(title[i].a.get('href'))
     
-#data2={'duration':[],'Description':[]}
-for i in range(len(title)):
-    r2=requests.get("https://www.imdb.com/{0}".format(link_list[i]))
-    soup2 = BeautifulSoup (r2.content,'lxml')
-    duration= soup2.find_all('li',attrs={'class':'ipc-inline-list__item'})
-    #data2['duration'].append(duration[2].text)
-    description= soup2.find('span',attrs={'class':'GenresAndPlot__TextContainerBreakpointXS_TO_M-cum89p-0 dcFkRD'})
-   # data2['Description'].append(description.text)
-    Api(movie_name=title[i].a.text,movie_year=title[i].span.text,movie_rating=ratings[i].strong.text,movie_duration=duration[2].text,movie_description=description.text).save()
+# #data2={'duration':[],'Description':[]}
+# for i in range(len(title)):
+#     r2=requests.get("https://www.imdb.com/{0}".format(link_list[i]))
+#     soup2 = BeautifulSoup (r2.content,'lxml')
+#     duration= soup2.find_all('li',attrs={'class':'ipc-inline-list__item'})
+#     #data2['duration'].append(duration[2].text)
+#     description= soup2.find('span',attrs={'class':'GenresAndPlot__TextContainerBreakpointXS_TO_M-cum89p-0 dcFkRD'})
+#    # data2['Description'].append(description.text)
+#     Api(movie_name=title[i].a.text,movie_year=title[i].span.text,movie_rating=ratings[i].strong.text,movie_duration=duration[2].text,movie_description=description.text).save()
 
 
 def index(request):
