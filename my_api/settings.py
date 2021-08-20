717cd002b50f252fbdae09309e66c68403659000
mkl-mukul <mukulmakwana09@gmail.com>
2021-08-20 17:01:56 +0530

    add new gui project with api

--- my_api/settings.py
+++ my_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     '_api',
+    'todo_movie',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- my_api/urls.py
+++ my_api/urls.py
@@ -21,4 +21,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/movie/',include("_api.urls")),
+    path('',include("todo_movie.urls")),
 ]
